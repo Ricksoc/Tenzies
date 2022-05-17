ffede2df390cf7b5a0b8309ad8d0f5674b4f1b1a
ricky <ricky.chaggar@gmail.com>
2022-05-17 12:02:21 +0100

    Create dice from array
    Set up allNewDice to generate an array of 10 numbers 1-6 and initilise
    a state with it.
    
    Map over array to create an array of dice components with values as
    the randomly generated numbers and key as array index.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Die from "./Die";
 
 export default function App() {
+  function allNewDice() {
+    return Array.from({ length: 10 }, () => Math.ceil(Math.random() * 6));
+  }
+
+  const [diceValues, setDiceValues] = useState(allNewDice());
+
+  const dice = diceValues.map((value, index) => {
+    return <Die key={index} value={value} />;
+  });
+
   return (
     <main>
-      <div className="dice">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={5} />
-        <Die value={4} />
-        <Die value={3} />
-        <Die value={2} />
-      </div>
+      <div className="dice">{dice}</div>
     </main>
   );
 }
